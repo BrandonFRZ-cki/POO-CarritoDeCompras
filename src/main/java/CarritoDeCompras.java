8995e5c55a2e2b1c39fd88d0aa4bd6e214a9375c
Crakchi <crakchi@gmail.com>
2025-06-09 12:24:45 -0500

    feat(UPS-001): First Commit. Creacion del programa base (Clases, atributos y Principal)

---
+++ src/main/java/CarritoDeCompras.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarritoDeCompras {
+    private List <ItemCarrito> listaItem;
+    public CarritoDeCompras() {
+        listaItem = new ArrayList<ItemCarrito>();
+    }
+    public void agregarProducto(Producto p, int cantidad) {
+        listaItem.add(new ItemCarrito(p, cantidad)); // Compocicion :)
+    }
+    public double calcularTotal() {
+        double total = 0;
+        for (ItemCarrito itemCarrito : listaItem) {
+            total+=itemCarrito.getCantidad()*itemCarrito.getProducto().getPrecio();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "CarritoDeCompras:\n" +
+                "\tItems:\n" + listaItem;
+    }
+}
---
+++ src/main/java/Principal.java
@@ -0,0 +1,24 @@
+public class Principal {
+    public static void main(String[] args) {
+        Producto p1 = new Producto("Producto Uno","CP-001",1.50);
+        Producto p2 = new Producto("Producto Dos","CP-002",2.50);
+        Producto p3 = new Producto("Producto Tres","CP-003",3.50);
+        Producto p4 = new Producto("Producto Cuatro","CP-004",4.50);
+        Producto p5 = new Producto("Producto Cinco","CP-005",5.50);
+        Producto p6 = new Producto("Producto Cinco","CP-006",6.50);
+        Producto p7 = new Producto("Producto Cinco","CP-007",7.50);
+
+        CarritoDeCompras carritoDeCompras = new CarritoDeCompras();
+        carritoDeCompras.agregarProducto(p1,1);
+        carritoDeCompras.agregarProducto(p2,2);
+        carritoDeCompras.agregarProducto(p3,3);
+        carritoDeCompras.agregarProducto(p4,4);
+        carritoDeCompras.agregarProducto(p5,5);
+        carritoDeCompras.agregarProducto(p6,6);
+        carritoDeCompras.agregarProducto(p7,7);
+
+        System.out.println(carritoDeCompras.toString());
+        System.out.println("Precio Total \n"+carritoDeCompras.calcularTotal());
+
+    }
+}
